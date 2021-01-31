84ca492c04b35e8116dccb51c1178d4b3fac0d08
KaranPatel7301 <60571680+KaranPatel7301@users.noreply.github.com>
2021-01-31 07:32:01 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.footballdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //button code
+
+
+        /// player Spinner
+        Spinner playerSpinner = (Spinner) findViewById(R.id.player_spinner);
+        ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.kansas_players));
+        playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        playerSpinner.setAdapter(playerAdapter);
+
+
+
+
+        //jersey selection spinner
+        Spinner numberSpinner = (Spinner) findViewById(R.id.number_spinner);
+        ArrayAdapter<String> NumberAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Chief_Jersey));
+        NumberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numberSpinner.setAdapter(NumberAdapter);
+
+        //team selection spinner
+        Spinner teamSpinner = (Spinner) findViewById(R.id.team_spinner);
+        ArrayAdapter<String> teamAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.team_spinner));
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(teamAdapter);
+        teamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+
+
+            {
+                @Override
+                public void onItemSelected (AdapterView < ? > parentView, View selectedItemView,
+                int position, long id){
+                changePlayerSpinner(playerSpinner, playerAdapter, teamSpinner.getSelectedItem().toString());
+                changeNumberSpinner(numberSpinner, NumberAdapter, teamSpinner.getSelectedItem().toString());
+            }
+
+                @Override
+                public void onNothingSelected (AdapterView < ? > parentView){
+                //do nothing
+            }
+                });
+
+    }
+
+    protected void changePlayerSpinner(Spinner playerSpinner, ArrayAdapter playerAdapter, String team) {
+        if(team.equals("Kansas City Chiefs")) {
+            playerAdapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.kansas_players));
+            playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            playerSpinner.setAdapter(playerAdapter);
+        } else if (team.equals("Tampa Bay Buccaneers")) {
+            playerAdapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.tampa_players));
+            playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            playerSpinner.setAdapter(playerAdapter);
+        }
+    }
+    protected void changeNumberSpinner(Spinner numberSpinner, ArrayAdapter NumberAdapter, String team) {
+        if(team.equals("Kansas City Chiefs")) {
+            NumberAdapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Chief_Jersey));
+            NumberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            numberSpinner.setAdapter(NumberAdapter);
+        } else if (team.equals("Tampa Bay Buccaneers")) {
+            NumberAdapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Tampa_Jersey));
+            NumberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            numberSpinner.setAdapter(NumberAdapter);
+        }
+    }
+}
